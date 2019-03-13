4b5e45ef8b34bab4b99a473f5ce2f2d70bf4afab
dylan <dylanspin100@hotmail.com>
2019-03-13 15:37:21 +0100

    update1
    achtergrond kan veranderen nu (maar een kleur en moet beter) moet dan nog om de beurt van kleur ver anderen

--- inleveropdrachten/jp.js
+++ inleveropdrachten/jp.js
@@ -1,10 +1,36 @@
-
-
-$("#div4").on("click", function() {
-    $(this).toggleClass('back-red');
-});
-
-
-function() {
-    $(this).css("background-color", "");
-};
+function Achtergrond1(){
+  var iets;
+document.getElementById("div").style.backgroundColor = "blue";
+}
+function Achtergrond2(){
+  var iets;
+document.getElementById("div2").style.backgroundColor = "blue";
+}
+function Achtergrond3(){
+  var iets;
+document.getElementById("div3").style.backgroundColor = "blue";
+}
+function Achtergrond4(){
+  var iets;
+document.getElementById("div4").style.backgroundColor = "blue";
+}
+function Achtergrond5(){
+  var iets;
+document.getElementById("div5").style.backgroundColor = "blue";
+}
+function Achtergrond6(){
+  var iets;
+document.getElementById("div6").style.backgroundColor = "blue";
+}
+function Achtergrond7(){
+  var iets;
+document.getElementById("div7").style.backgroundColor = "blue";
+}
+function Achtergrond8(){
+  var iets;
+document.getElementById("div8").style.backgroundColor = "blue";
+}
+function Achtergrond9(){
+  var iets;
+document.getElementById("div9").style.backgroundColor = "blue";
+}
