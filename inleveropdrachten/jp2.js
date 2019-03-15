da7bcb0623149bbe581c6f844197cc4dacdd33ff
dylan <dylanspin100@hotmail.com>
2019-03-15 13:05:48 +0100

    update1
    dit is een test voor de arrow keys

--- inleveropdrachten/jp2.js
+++ inleveropdrachten/jp2.js
@@ -1,4 +1,40 @@
-function block(){
 
+
+  function Block(Block) {
+
+  var loc = 2.3;
   
+      LINKS = 37,
+      RECHTS = 39,
+      BOVEN = 38,
+      BENEDEN = 40;
+
+      switch (Block) {
+      case LINKS:
+       document.getElementById(1.1).style.backgroundColor = "#1B63DF";
+        break;
+      case RECHTS:
+       document.getElementById(1.2).style.backgroundColor = "#1B63DF";
+        break;
+      case BOVEN:
+       document.getElementById(1.3).style.backgroundColor = "#1B63DF";
+        break;
+      case BENEDEN:
+       document.getElementById(1.4).style.backgroundColor = "#1B63DF";
+         break;
+      default:
+       document.querySelector("#geen").innerHTML = "<h1>geen arrow key</h1>";
+         break;
+    }
+
 }
+
+    function check(event) {
+      var indruk = event || window.event,
+        knop = (indruk.which) ? indruk.which : indruk.keyCode;
+
+        Block(knop);
+
+        return false;
+    }
+    document.onkeydown = check;
