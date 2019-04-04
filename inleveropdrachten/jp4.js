4be60b4cce5be04c3db2cde43f2c4fbc205edaec
dylan <dylanspin100@hotmail.com>
2019-04-04 11:34:47 +0200

    regex update
    de engelse en de nederlandse pagina zijn klaar.
    
    dus nu is de opdracht klaar als het goed is.

--- inleveropdrachten/jp4.js
+++ inleveropdrachten/jp4.js
@@ -1,10 +1,5 @@
 
-var naam = document.getElementById('0');
-var adres = document.getElementById('1');
-var woon = document.getElementById('2');
-var postcode = document.getElementById('3');
-var land = document.getElementById('4');
-var beroep = document.getElementById('beroep');
+
 
    var click = true;
     function select(){
@@ -48,17 +43,98 @@ var beroep = document.getElementById('beroep');
           }
             info.style.opacity = op;
             info.style.filter = 'alpha(opacity=' + op * 100 + ")";
-            op += op * 0.005;
+            op += op * 0.005; //timer
         }, 10);
       }
 
- function naam(){
-   var r =/^[a-zA-Z]/;
-   console.log(r.test(naam));
-     if(r){
+ function check(){
+   var naam = document.getElementById('0').value;
+   var adres = document.getElementById('1').value;
+   var woon = document.getElementById('2').value;
+   var postcode = document.getElementById('3').value;
+   var land = document.getElementById('4').value;
+   var beroep = document.getElementById('6').value;
 
-     }
+   var r =/[a-zA-Z]+/g;
+   var adr = /^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i;
+   var post = /^[1-9][0-9]{3}[\s]?[A-Za-z]{2}$/i;
+   var i = (r.test(naam));
+   var l = (adr.test(adres));
+   var p = (post.test(postcode));
+   var l = (r.test(land));
+   var w = (r.test(woon));
+   var b = (r.test(beroep));
+
+     console.log(beroep);
+     console.log(b);
+     if(i){}
      else{
-       window.alert("Geboortedatum");
+       window.alert("geen Naam");
+     }
+     if(l){}
+     else{
+       window.alert("geen Adres");
+     }
+     if(p){}
+     else{
+       window.alert("geen Woonplaats");
+     }
+     if(p){}
+     else{
+       window.alert("geen Postcode");
+     }
+     if(l){}
+     else{
+       window.alert("geen Land");
+     }
+     if(b){}
+     else{
+       window.alert("geen beroep");
+     }
+ }
+
+ function check1(){
+   var naam = document.getElementById('0').value;
+   var adres = document.getElementById('1').value;
+   var woon = document.getElementById('2').value;
+   var postcode = document.getElementById('3').value;
+   var land = document.getElementById('4').value;
+   var beroep = document.getElementById('6').value;
+
+   var r =/[a-zA-Z]+/g;
+   var adr = /^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i;
+   var post = /^[1-9][0-9]{3}[\s]?[A-Za-z]{2}$/i;
+   var i = (r.test(naam));
+   var l = (adr.test(adres));
+   var p = (post.test(postcode));
+   var l = (r.test(land));
+   var w = (r.test(woon));
+   var b = (r.test(beroep));
+
+     console.log(beroep);
+     console.log(b);
+     if(i){}
+     else{
+       window.alert("no Name");
+     }
+     if(l){}
+     else{
+       window.alert("no Address");
+     }
+     if(p){}
+     else{
+       window.alert("no residence");
+     }
+     if(p){}
+     else{
+       window.alert("no Zip code");
+     }
+     if(l){}
+     else{
+       window.alert("no Country");
+     }
+     if(b){}
+     else{
+       window.alert("no Job");
      }
  }
